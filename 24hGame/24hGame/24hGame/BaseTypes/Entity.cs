a7d97f50f3526097a4a178c639a6931c3f8d705c
snellface <tom.almgren@gmail.com>
2013-12-07 15:29:48 +0100

    Basic file structor

---
+++ 24hGame/24hGame/24hGame/BaseTypes/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _24hGame.BaseTypes
+{
+    class Entity
+    {
+        static public Entity()
+        {
+            random = new Random();
+        }
+        static Random random;
+        static Random Random
+        {
+            get
+            {
+                return random;
+            }
+        }
+    }
+}
---
+++ 24hGame/24hGame/24hGame/BaseTypes/SoundEffectCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _24hGame.BaseTypes
+{
+    class SoundEffectCollection : Entity
+    {
+        List<SoundEffect> sounds;
+        public List<SoundEffect> Sounds
+        {
+            get
+            {
+                return sounds;
+            }
+            set
+            {
+                sounds = value;
+            }
+        }
+
+        public void PlayRandom()
+        {
+
+        }
+    }
+}
