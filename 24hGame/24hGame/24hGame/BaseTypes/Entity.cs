89dc7b344da0e735a2ffb3ad94f2e53f56633036
Nick <angelltlf@gmail.com>
2013-12-07 18:20:51 +0100

    add de/serilization into Entity

--- 24hGame/24hGame/24hGame/BaseTypes/Entity.cs
+++ 24hGame/24hGame/24hGame/BaseTypes/Entity.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
 namespace _24hGame.BaseTypes
 {
-    class Entity
+    public class Entity
     {
-        static Entity()
+        public void Serialize<Entity>(Entity data, string filePath)
         {
-            random = new Random();
-        }
-        static Random random;
-        static Random Random
-        {
-            get
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Entity));
+            //Overridden to use UTF8 for compatability with Perl XML::DOM
+
+            using (TextWriter writer = new StreamWriter(filePath))
             {
-                return random;
+                xmlSerializer.Serialize(writer, data);
             }
         }
+
+        public Entity DeSerilize(Entity data, string filePath)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Entity));
+
+            using (XmlReader reader = XmlReader.Create(filePath))
+            {
+                data = (Entity)ser.Deserialize(reader);
+            }
+            return data;
+        }
     }
+    
 }
