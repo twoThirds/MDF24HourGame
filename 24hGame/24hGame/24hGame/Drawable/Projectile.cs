6ec90fb1629ca650e3ab4357cce9461656e0367a
björn <bjorn.delight@gmail.com>
2013-12-08 05:30:25 +0100

    updated projectile stuff

--- 24hGame/24hGame/24hGame/Drawable/Projectile.cs
+++ 24hGame/24hGame/24hGame/Drawable/Projectile.cs
@@ -15,13 +15,16 @@ namespace _24hGame.Drawable
 		Vector2 direction;
 		Game1 game;
 		Room containingRoom;
+		float velocity;
         float projectileWidth; // Used for finding hits
 
-		public Projectile(Vector2 direction, Game1 game, Room containingRoom)
+		public Projectile(Vector2 direction, Game1 game, Room containingRoom, float velocity)
         {
 			this.direction = direction;
+			this.direction.Normalize();
 			this.game = game;
 			this.containingRoom = containingRoom;
+			this.velocity = velocity;
 			Position = new Vector2(50, 50);
 			Texture = new TexturedQuad();
 			Texture.Texture = game.Content.Load<Texture2D>(@"Textures\ui\pistol bullet");
@@ -30,8 +33,8 @@ namespace _24hGame.Drawable
 		{
 			float distance = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000.0f;
 			Vector2 traveled = new Vector2(direction.X, direction.Y);
-			traveled.X *= distance;
-			traveled.Y *= distance;
+			traveled.X *= distance*velocity;
+			traveled.Y *= distance*velocity;
 			Position += traveled;
 			//dev
 			if(Position.X > containingRoom.RoomSize.X || Position.Y > containingRoom.RoomSize.Y)
